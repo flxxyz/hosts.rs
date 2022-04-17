26bbb99c82b1d54768185b322acb1c10dad678d7
flxxyz <i@mdzz.name>
2022-04-17 23:57:10 +0800

    feat: 支持合并订阅内容并写入指定文件

--- core/lib.rs
+++ core/lib.rs
@@ -1,12 +1,30 @@
 pub const DB_PATH_NAME: &str = ".hosts";
 
-fn db() -> sled::Db {
+pub fn db() -> sled::Db {
   // 获得当前用户的home目录
   let home_dir = dirs::home_dir().unwrap();
   // 拼接本地数据库存储路径
   let db_path = home_dir.join(crate::DB_PATH_NAME);
+  // 创建数据库
+  let db = sled::open(db_path).unwrap();
+  // 设置合并函数
+  db.set_merge_operator(concatenate_merge);
   // 返回数据库实例
-  return sled::open(db_path).unwrap();
+  return db;
+}
+
+fn concatenate_merge(
+  _key: &[u8],               // the key being merged
+  old_value: Option<&[u8]>,  // the previous value, if one existed
+  merged_bytes: &[u8]        // the new bytes being merged in
+) -> Option<Vec<u8>> {       // set the new value, return None to delete
+  let mut ret = old_value
+    .map(|ov| ov.to_vec())
+    .unwrap_or_else(|| vec![]);
+
+  ret.extend_from_slice(merged_bytes);
+
+  Some(ret)
 }
 
 // 插入一条数据
@@ -30,4 +48,42 @@ pub fn find() -> Vec<String> {
     values.push(String::from_utf8_lossy(&k).to_string());
   }
   return values;
+}
+
+pub fn save(key: &str, value: String) {
+  match db().insert(key, value.to_string().into_bytes()) {
+    Ok(_) => {
+      println!("保存成功✌️ {}", value);
+    }
+    Err(_) => {
+      println!("保存失败👽 {}", value);
+    }
+  }
+}
+
+pub fn merge(key: &str, values: Vec<String>) -> String {
+  for (index, v) in values.iter().enumerate() {
+    let res: sled::Result<Option<sled::IVec>>;
+    let nv = (v.to_string() + "\n").into_bytes();
+    if index == 0 {
+      res = db().insert(key, nv);
+    } else {
+      res = db().merge(key, nv);
+    }
+
+    match res {
+      Ok(_) => {}
+      Err(_) => {}
+    }
+  }
+
+  let value = db().get(key).unwrap().unwrap();
+  
+  // 清理保存的数据，不在乎是否成功
+  match db().remove(key) {
+    Ok(_) => {}
+    Err(_) => {}
+  }
+
+  return String::from_utf8_lossy(&value).to_string();
 }
--- src/fetch.rs
+++ src/fetch.rs
@@ -1,57 +1,127 @@
 use clap::{Arg, ArgMatches, Command};
+use std::fs::File;
+use std::io;
+use std::io::{Read, Write};
+use std::path::Path;
 
 pub const COMMAND_NAME: &str = "fetch";
 const MIRROR_URL: &str = "https://ghproxy.com/"; // 使用的镜像地址
+const DEFAULT_FILENAME: &str = "/etc/hosts";
+const START_TAG: &str = "# hosts.rs";
+const REPO_TAG: &str = "# repo: https://github.com/flxxyz/hosts.rs";
 
 pub fn command() -> Command<'static> {
     let args_mirror = Arg::new("mirror")
         .short('m')
         .long("mirror")
         .help("使用镜像地址");
+    let args_output = Arg::new("output")
+        .short('o')
+        .long("output")
+        .help("直接输出内容");
+    let args_filename = Arg::new("filename")
+        .short('f')
+        .long("filename")
+        .takes_value(true)
+        .help("指定输出的路径参数, 默认: /etc/hosts");
+    let args_log = Arg::new("log").short('l').long("log").help("输出日志");
     let args_help = Arg::new("help").short('h').long("help").help("帮助信息");
 
     Command::new(COMMAND_NAME)
         .about("开始抓取hosts")
         .alias("f")
         .arg(args_mirror)
+        .arg(args_output)
+        .arg(args_filename)
+        .arg(args_log)
         .arg(args_help)
 }
 
 pub fn handle(matches: &ArgMatches) -> &ArgMatches {
+    let is_log = matches.is_present("log");
+
     let mut url_prefix = String::from("");
     if matches.is_present("mirror") {
         url_prefix = String::from(MIRROR_URL);
     }
 
+    let mut responses = vec![format!("{}\n{}\n", START_TAG, REPO_TAG)];
     let urls = core::find();
     for url in urls.iter() {
         let res = reqwest::blocking::get(format!("{}{}", url_prefix, url));
         match res {
             Err(_) => {
-                println!("抓取失败💩 {}", url);
+                if is_log {
+                    println!("抓取失败💩 {}", url);
+                }
             }
             Ok(response) => {
                 if response.status().is_success() {
-                    println!("抓取成功✌️ {}", url);
-                    save(response.text().unwrap());
+                    if is_log {
+                        println!("抓取成功🎉 {}", url);
+                    }
+                    responses.push(response.text().unwrap());
                 } else {
-                    println!("抓取失败💩 {}", url);
+                    if is_log {
+                        println!("抓取失败😭 {}", url);
+                    }
                 }
             }
         };
     }
 
-    merge_and_write_file();
+    let mut hosts = core::merge("response", responses);
+    if matches.is_present("output") {
+        println!("{}", hosts);
+    } else {
+        let mut hosts_lines: Vec<&str> = hosts.split("\n").collect();
+        let filename = matches.value_of("filename").unwrap_or(DEFAULT_FILENAME);
+        let content = cat(filename).unwrap_or_default();
+
+        let mut lines: Vec<&str> = vec![];
+
+        if content == "" {
+            lines = hosts_lines;
+        } else {
+            let content_lines: Vec<&str> = content.split("\n").collect();
+            for line in content_lines.iter() {
+                if line.starts_with(START_TAG) {
+                    break;
+                }
+                lines.push(line);
+            }
+            lines.append(&mut hosts_lines);
+        }
+
+        hosts = lines.join("\n");
+
+        match echo(hosts.as_str(), filename) {
+            Ok(_) => {
+                if is_log {
+                    println!("写入成功🎉 {}", filename);
+                }
+            }
+            Err(err) => {
+                if is_log {
+                    println!("写入失败😭 Err: {}", err);
+                }
+            }
+        }
+    }
 
     return matches;
 }
 
-fn save(_: String) {
-    // TODO: 保存抓取的hosts
+fn cat(path: &str) -> io::Result<String> {
+    let mut f = File::open(Path::new(path))?;
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
 
-fn merge_and_write_file() {
-    // TODO: 合并抓取到的hosts
-    // TODO: 并写入本地hosts文件，可指定路径
-    // TODO: 清理save()临时保存hosts
+fn echo(s: &str, path: &str) -> io::Result<()> {
+    let mut f = File::create(Path::new(path))?;
+    f.write_all(s.as_bytes())
 }
